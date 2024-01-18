451a8215e264d14c5f9983ea556d25918bf07ed0
Nthntf <theo.feu54@gmail.com>
2024-01-18 13:37:12 +0100

    added template for card in JS

--- script/callApi.js
+++ script/callApi.js
@@ -1,9 +1,11 @@
 let searchBtn = document.querySelector("#searchBtn");
 let countryCard = document.querySelector("#countryCard");
+let cardContainer = document.querySelector("#cardContainer")
 let searchBar = document.querySelector("#country-search");
 let searchBarValue = "";
 
 searchBar.addEventListener('keyup', () => {
+    clearHTML();
     getCountriesByName();
 });
 
@@ -13,9 +15,21 @@ searchBar.addEventListener('keyup', () => {
 
 async function getCountriesByName() {
     searchBarValue = searchBar.value;
-    let response = await fetch(`https://restcountries.com/v3.1/name/${searchBarValue}`);
+    const response = await fetch(`https://restcountries.com/v3.1/name/${searchBarValue}`);
     if (response.ok) {
-        let countriesObject = await response.json();
-        console.log(countriesObject[0].name.common);
+        let countriesData = await response.json();
+        console.log(countriesData);
+        for (let i = 0; i < 10; i++) {
+            if (countriesData[i]?.name?.common) {
+                cardContainer.innerHTML += `<div>
+                                            <p>Nom: ${countriesData[i].name.common}</p>
+                                        </div>`
+            }
+        }
     }
+}
+
+function clearHTML() {
+    // évite l'addition des résultats de toutes les recherches
+    cardContainer.innerHTML = '';
 }
