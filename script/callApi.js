5d0e9b9e4c7825376dd2d29ee1fc3282e297a8d5
Nthntf <theo.feu54@gmail.com>
2024-01-18 14:09:15 +0100

    cards are now displayed in html with countries data

--- script/callApi.js
+++ script/callApi.js
@@ -19,11 +19,18 @@ async function getCountriesByName() {
     if (response.ok) {
         let countriesData = await response.json();
         console.log(countriesData);
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 12; i++) {
             if (countriesData[i]?.name?.common) {
-                cardContainer.innerHTML += `<div>
-                                            <p>Nom: ${countriesData[i].name.common}</p>
-                                        </div>`
+                cardContainer.innerHTML += `<div class="card">
+                                                <div class="card-img">
+                                                <img src="${countriesData[i].flags.png}" alt="">
+                                                </div>
+                                                <div class="card-info">
+                                                <h1>${countriesData[i].name.common}</h1>
+                                                <p>Capitale : ${countriesData[i].capital}</p>
+                                                <p>Population : ${countriesData[i].population}</p>
+                                                </div>
+                                            </div>`
             }
         }
     }
