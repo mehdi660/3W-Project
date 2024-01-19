74140828b2c6013fc582d1e6fafb5f3a7a98ae51
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-01-19 10:27:09 +0100

    :hammer: population filter is done

--- script/callApi.js
+++ script/callApi.js
@@ -1,35 +1,32 @@
 let searchBtn = document.querySelector("#searchBtn");
 let cleanBtn = document.querySelector("#cleanBtn");
-let countryCard = document.querySelector("#countryCard");
 let cardContainer = document.querySelector("#cardContainer");
 let searchBar = document.querySelector("#country-search");
 let searchBarValue = "";
 let url = `https://restcountries.com/v3.1/all`;
 
 document.addEventListener("DOMContentLoaded", () => {
-    // show all countries by default
-    getCountriesByName();
+  // show all countries by default
+  getCountriesByName();
 });
 
 searchBar.addEventListener("keydown", (e) => {
-    // call api when enter key is pressed
-    if (e.key === "Enter" || e.keyCode === 13) {
-        searchActivation();
-        e.preventDefault();
-    }
+  // call api when enter key is pressed
+  if (e.key === "Enter" || e.keyCode === 13) {
+    searchActivation();
+    e.preventDefault();
+  }
 });
 
 searchBtn.addEventListener("click", () => {
-    // call api when click on the search button
-    searchActivation();
-    e.preventDefault();
+  // call api when click on the search button
+  searchActivation();
 });
 
 cleanBtn.addEventListener("click", () => {
-    // show all countries like default
-    url = `https://restcountries.com/v3.1/all`
-    searchActivation();
-    e.preventDefault();
+  // show all countries like default
+  url = `https://restcountries.com/v3.1/all`;
+  searchActivation();
 });
 
 async function getCountriesByName() {
@@ -37,18 +34,28 @@ async function getCountriesByName() {
   if (response.ok) {
     let countriesData = await response.json();
     console.log(countriesData);
+
+    // nombre max d'affichage sur la page
+    const maxDisplayCount = 30;
+    let displayCount = 0;
+
     for (let i = 0; i < countriesData.length; i++) {
-      if (countriesData[i]?.name?.common) {
+      if (countriesData[i]?.name?.common && displayCount < maxDisplayCount) {
         cardContainer.innerHTML += `<div class="card">
-                                                <div class="card-img">
-                                                <img src="${countriesData[i].flags.png}" alt="">
-                                                </div>
-                                                <div class="card-info">
-                                                <h1>${countriesData[i].name.common}</h1>
-                                                <p>Capitale : ${countriesData[i].capital}</p>
-                                                <p>Population : ${countriesData[i].population.toLocaleString()}</p>
-                                                </div>
-                                            </div>`;
+                    <div class="card-img">
+                        <img src="${countriesData[i].flags.png}" alt="">
+                    </div>
+                    <div class="card-info">
+                        <h1>${countriesData[i].name.common}</h1>
+                        <p>Capitale : ${countriesData[i].capital}</p>
+                        <p>Population : ${countriesData[
+                          i
+                        ].population.toLocaleString()}</p>
+                    </div>
+                </div>`;
+
+        // incremente le compteur daffichages
+        displayCount++;
       }
     }
   }
@@ -60,10 +67,10 @@ function clearHTML() {
 }
 
 function searchActivation() {
-    searchBarValue = searchBar.value;
-    if (searchBarValue.length !== 0) {
-        url = `https://restcountries.com/v3.1/name/${searchBarValue}`;
-    }
-    clearHTML();
-    getCountriesByName();
+  searchBarValue = searchBar.value;
+  if (searchBarValue.length !== 0) {
+    url = `https://restcountries.com/v3.1/name/${searchBarValue}`;
+  }
+  clearHTML();
+  getCountriesByName();
 }
